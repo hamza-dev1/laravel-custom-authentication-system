c3c0cb44c5be99a26caffba84723e39a1748a4c8
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-10 23:03:01 +0100

    Register AuthVerification and AlreadyLoggedIn

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,6 +53,8 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'loggedIn' => \App\Http\Middleware\AlreadyLoggedIn::class,
+        'authVerify' => \App\Http\Middleware\AuthVerification::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
