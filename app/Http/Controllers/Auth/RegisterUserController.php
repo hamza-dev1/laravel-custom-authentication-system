d17ae4827e0290d5be3e2d1e96a611fe4128b21b
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-10 23:05:52 +0100

    Controller for Registeration functionality

--- app/Http/Controllers/Auth/RegisterUserController.php
+++ app/Http/Controllers/Auth/RegisterUserController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\CustomUser;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterUserController extends Controller
 {
@@ -16,8 +20,20 @@ class RegisterUserController extends Controller
         $request->validate([
             'firstname' => ['required', 'string', 'max:50'],
             'lastname' => ['required', 'string', 'max:50'],
-            'email' => ['required', 'string', 'max:200'],
-            'password' => ['required', 'string'],
+            'email' => ['required', 'string', 'max:200', 'unique:custom_users'], // check also for uniqueness of email
+            'password' => ['required', 'string', 'min:8'],
         ]);
+
+        $user = CustomUser::create([
+            'first_name' => $request->firstname,
+            'last_name' => $request->lastname,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $request->session()->flash('message', 'You account created successfully.');
+        Auth::login($user); // perform login for every new user
+
+        return redirect(RouteServiceProvider::HOME);
     }
 }
