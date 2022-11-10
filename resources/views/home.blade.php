9dd9ec4e8855108f7c9084253837c0450b22ffa7
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-10 23:09:49 +0100

    Add logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,8 @@
 @section('title', 'Home')
 
 @section('content')
-
-
+    <div class="container mx-auto">
+            <img src="{{ asset('images/laravel-logo.svg') }}" alt="laravel logo">
+    </div>
 
 @endsection
