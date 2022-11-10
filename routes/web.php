037cdde7ed5ccfe15ce9a8c097746e01c7037c51
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-10 23:05:15 +0100

    Add routes for Authentication process, home and dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Auth\RegisterUserController;
 use App\Http\Controllers\Auth\LoginUserController;
+use App\Http\Controllers\Auth\LogoutUserController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +20,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/login', [LoginUserController::class, 'login_page'])->name('login');
+Route::get('/login', [LoginUserController::class, 'login_page'])->middleware('loggedIn')->name('login');
 Route::get('/register', [RegisterUserController::class, 'register_page'])->name('register');
 
 Route::post('/login', [LoginUserController::class, 'login_user'])->name('login-user');
 Route::post('/register', [RegisterUserController::class, 'register_user'])->name('register-user');
+
+Route::post('/logout', [LogoutUserController::class, 'logout'])->middleware('authVerify')->name('logout');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('authVerify')->name('dashboard');
