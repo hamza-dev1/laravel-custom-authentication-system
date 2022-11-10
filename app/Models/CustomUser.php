a3aeeb30b52fab213d253b4da2592176384e9860
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-10 23:03:51 +0100

    Customized user model

--- app/Models/CustomUser.php
+++ app/Models/CustomUser.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class CustomUser extends Model
+class CustomUser extends Authenticatable
 {
     use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password'
+    ];
+
 }
